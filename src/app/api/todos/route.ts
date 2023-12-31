f34a4e1e548c42ebaa8dd2cc5b61754c3cc07bb0
Kiptanui Boaz <boaserem022@gmail.com>
2023-12-31 13:15:00 +0300

    added delete route

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -4,10 +4,28 @@ const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
 
 const API_KEY: string = process.env.DATA_API_KEY as string
 
+//get route
 export async function GET() {
     const res = await fetch(DATA_SOURCE_URL)
 
     const todos: Todo[] = await res.json()
 
     return NextResponse.json(todos)
+}
+
+//delete routeexport async function DELETE(request: Request) {
+export async function DELETE(request: Request) {
+    const { id }: Partial<Todo> = await request.json()
+
+    if (!id) return NextResponse.json({ "message": "Todo id required" })
+
+    await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        }
+    })
+
+    return NextResponse.json({ "message": `Todo ${id} deleted` })
 }
